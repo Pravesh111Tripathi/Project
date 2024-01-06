6b2b586ccee68d9b0b8dd945fca1ef9037d0d59a
praveshtripathi <pravesh111tripathi@gmail.com>
2024-01-06 17:10:53 +0530

    Initial commit

--- src/tutorial/FilteringConditions.java
+++ src/tutorial/FilteringConditions.java
@@ -1,9 +1,6 @@
 package tutorial;
-<<<<<<< HEAD
 
-=======
 @FunctionalInterface
->>>>>>> 2e9b36a (Initial commit)
 public interface FilteringConditions {
-    abstract boolean test(Hotel hotel);
+    boolean test(Hotel hotel);
 }
